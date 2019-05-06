e0e0100f0a4229ed6151ed2b2b36c9cca64d7ae2
wangbingzhigit <wbz853244870@outlook.com>
2019-05-07 00:15:17 +0800

    springboot集成redis

---
+++ springbootwithredis/src/main/java/com/ispringbootredis/SpringbootwithredisApplication.java
@@ -0,0 +1,15 @@
+package com.ispringbootredis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class SpringbootwithredisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwithredisApplication.class, args);
+    }
+
+}
